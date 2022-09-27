f4f0df32d7f13b66d864c85bb7a834e000e5b067
Atul Jha <atuljha062@gmail.com>
2022-09-27 22:39:44 +0530

    adding bubble sort

---
+++ bubbleSort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=1;j<n-i;j++){
+            if(arr[j]<arr[j-1]){
+                swap(arr[j],arr[j-1]);
+            }
+        }
+    }
+}
+
+void printArr(int arr[], int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    int arr[] = {5,6,1,4,17,11};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Before Sorting"<<endl;
+
+    printArr(arr,n);
+
+    bubbleSort(arr,n);
+
+    cout<<"After sorting"<<endl;
+
+    printArr(arr,n);
+
+    return 0;
+}
